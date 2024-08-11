6b8582fecb8c952d7eea980ed9d3d140d9399689
keplib <keplib@gmail.com>
2024-08-11 17:42:39 +0200

    Refactor

--- notes/index.js
+++ notes/index.js
@@ -1,30 +1,19 @@
-require('dotenv').config();
-const { Sequelize, QueryTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { PORT } = require('./util/config');
+const { connectToDatabase } = require('./util/db');
 
-app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query('SELECT * FROM notes', { type: QueryTypes.SELECT });
-  res.json(notes);
-});
+const notesRouter = require('./controllers/notes');
 
-const PORT = process.env.port || 3001;
+app.use(express.json());
+app.use('/api/notes', notesRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-// const main = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     const notes = await sequelize.query('SELECT * FROM notes', { type: QueryTypes.SELECT });
-//     console.log(notes);
-//     sequelize.close();
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// };
-
-// main();
+start();
