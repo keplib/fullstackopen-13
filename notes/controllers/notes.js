d99a8a9f6401127486d84ad7dd2c8e05f71ad82e
keplib <keplib@gmail.com>
2024-08-20 11:20:53 +0200

    remove unused code

--- notes/controllers/notes.js
+++ notes/controllers/notes.js
@@ -5,27 +5,6 @@ const { SECRET } = require('../util/config');
 const { Note, User } = require('../models');
 const { tokenExtractor, noteFinder } = require('../util/middleware');
 
-// const noteFinder = async (req, res, next) => {
-//   req.note = await Note.findByPk(req.params.id);
-//   next();
-// };
-
-// const tokenExtractor = (req, res, next) => {
-//   const authorization = req.get('authorization');
-
-//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-//     try {
-//       req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-//       console.log(req.decodedToken);
-//     } catch {
-//       return res.status(401).json({ error: 'token invalid' });
-//     }
-//   } else {
-//     return res.status(401).json({ error: 'token missing' });
-//   }
-//   next();
-// };
-
 router.get('/', async (req, res) => {
   const notes = await Note.findAll({
     attributes: { exclude: ['userId'] },
--- notes/controllers/users.js
+++ notes/controllers/users.js
@@ -4,30 +4,6 @@ const { User, Note } = require('../models');
 
 const { tokenExtractor, isAdmin } = require('../util/middleware');
 
-// const isAdmin = async (req, res, next) => {
-//   const user = await User.findByPk(req.decodedToken.id);
-//   if (!user.admin) {
-//     return res.status(401).json({ error: 'operation not allowed' });
-//   }
-//   next();
-// };
-
-// const tokenExtractor = (req, res, next) => {
-//   const authorization = req.get('authorization');
-
-//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-//     try {
-//       req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-//       console.log(req.decodedToken);
-//     } catch {
-//       return res.status(401).json({ error: 'token invalid' });
-//     }
-//   } else {
-//     return res.status(401).json({ error: 'token missing' });
-//   }
-//   next();
-// };
-
 router.get('/', async (req, res) => {
   const users = await User.findAll({
     include: {
